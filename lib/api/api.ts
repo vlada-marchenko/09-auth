1c50b2c5ce498d8d3dddaac594b90ffd53d15be7
vlada-marchenko <vvvvadya44@gmail.com>
2025-07-17 21:38:21 +0200

    fix url

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 export const nextServer = axios.create({
   baseURL: baseURL,
