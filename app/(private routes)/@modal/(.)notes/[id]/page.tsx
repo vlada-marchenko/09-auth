8864c337e475b93a2905520d7bbde90539d0f10b
vlada-marchenko <vvvvadya44@gmail.com>
2025-07-17 20:09:13 +0200

    fifth

--- app/(private routes)/@modal/(.)notes/[id]/page.tsx
+++ app/(private routes)/@modal/(.)notes/[id]/page.tsx
@@ -1,6 +1,6 @@
-import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
-import { QueryClient } from '@tanstack/react-query'
-import { fetchNoteById } from '../../../../../lib/api/clientApi'
+// import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
+// import { QueryClient } from '@tanstack/react-query'
+// import { fetchNoteById } from '../../../../../lib/api/clientApi'
 import NotePreview from './NotePreview.client'
 
 export default async function NoteModalPage({
@@ -10,16 +10,16 @@ export default async function NoteModalPage({
 }) {
   const { id } = await params
 
-  const queryClient = new QueryClient()
-  await queryClient.prefetchQuery({
-    queryKey: ['note', id],            
-    queryFn: () => fetchNoteById(id),
-  })
+  // const queryClient = new QueryClient()
+  // await queryClient.prefetchQuery({
+  //   queryKey: ['note', id],            
+  //   queryFn: () => fetchNoteById(id),
+  // })
 
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
+    // <HydrationBoundary state={dehydrate(queryClient)}>
       <NotePreview noteId={id} />
-    </HydrationBoundary>
+    // </HydrationBoundary>
   )
 }
